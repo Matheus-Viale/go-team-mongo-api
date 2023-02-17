cdc8c6d9a3b2c47d3894e719aa32ac5332b95ede
Matheus-Viale <72235952+Matheus-Viale@users.noreply.github.com>
2023-02-17 03:22:20 -0300

    Update server.js

--- dist/server.js
+++ dist/server.js
@@ -3,11 +3,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const mongoose_1 = require("mongoose");
 const routes_1 = require("./routes");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 mongoose_1.default.connect('mongodb+srv://discordbot:6cK8k44ITR6mmXor@discordbot.gh611mu.mongodb.net/?retryWrites=true&w=majority').catch(console.error);
 app.use(express.json());
 app.use(routes_1.default);
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    app.use(cors());
+    next();
+});
 app.listen(port, () => {
     console.log('Server is listening');
 });
